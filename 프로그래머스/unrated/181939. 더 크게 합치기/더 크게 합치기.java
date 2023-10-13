31a6c4f40b0ba0887968afeed5ed85a798d38b14
Doreusia <129160905+Doreusia@users.noreply.github.com>
2023-10-14 00:11:55 +0900

    [level unrated] Title: 더 크게 합치기, Time: 1.25 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,8 +1,7 @@
 class Solution {
     public int solution(int a, int b) {
-        String ab = String.valueOf(a) + String.valueOf(b);
-        String ba = String.valueOf(b) + String.valueOf(a);
-        
-        return Integer.parseInt(ab) >= Integer.parseInt(ba) ? Integer.parseInt(ab) : Integer.parseInt(ba);
+        int ab = Integer.parseInt(Integer.toString(a) + Integer.toString(b));
+        int ba = Integer.parseInt(Integer.toString(b) + Integer.toString(a));
+        return ab >= ba ? ab : ba;
     }
 }
